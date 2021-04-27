9e73f9c78a9b34c1269303cecb5b82a713c6520b
丁学紧 <accounts_5a503b085f65ba798cb5b115@mail.teambition.com>
2021-04-27 11:57:16 +0800

    fix:修复webpack命令报错问题

--- example/config/webpack.config.js
+++ example/config/webpack.config.js
@@ -2,14 +2,14 @@
  * @Description:
  * @Author: dingxuejin
  * @Date: 2021-02-01 14:53:16
- * @LastEditTime: 2021-02-01 17:32:00
+ * @LastEditTime: 2021-04-27 11:39:31
  * @LastEditors: dingxuejin
  */
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const { setEntryAndHtmlPlugin } = require("webpackUtils");
+const { setEntryAndHtmlPlugin } = require("../../index");
 
 const entryFile = ["page-1", "page-2", "home"];
 
--- example/demo.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Description:
- * @Author: dingxuejin
- * @Date: 2021-02-01 16:33:26
- * @LastEditTime: 2021-02-01 16:33:38
- * @LastEditors: dingxuejin
- */
-const { setEntryAndHtmlPlugin } = require("webpackUtils");
-console.log(setEntryAndHtmlPlugin);
